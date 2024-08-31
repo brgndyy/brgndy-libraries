3c658eedf60cb745df7abf5c3e462c628f0e5ac3
brgndy <brgndy96@gmail.com>
2024-09-01 00:36:55 +0900

    test: 테스트 콘솔 삭제

--- packages/react-metronome/src/App.tsx
+++ packages/react-metronome/src/App.tsx
@@ -1,7 +1,6 @@
 import Metronome from './lib/components/Metronome/Metronome';
 
 export default function App() {
-  console.log('test console.');
   return (
     <Metronome>
       <Metronome.BPMInput />
