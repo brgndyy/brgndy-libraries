1e2185e3afc9889a55beb98f45098e5e7c03b2f5
brgndy <brgndy96@gmail.com>
2024-09-01 00:31:35 +0900

    test: 테스트 콘솔 추가

--- packages/react-metronome/src/App.tsx
+++ packages/react-metronome/src/App.tsx
@@ -1,6 +1,7 @@
 import Metronome from './lib/components/Metronome/Metronome';
 
 export default function App() {
+  console.log('test console.');
   return (
     <Metronome>
       <Metronome.BPMInput />
