f46f8931db4717ecddf0d79c84d5f7f87302f53a
brgndy <brgndy96@gmail.com>
2024-10-08 01:23:25 +0900

    chore: 재배포

--- packages/react/tanstack-query/rollup.config.js
+++ packages/react/tanstack-query/rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import terser from '@rollup/plugin-terser';
 
@@ -8,26 +9,33 @@ import packageJson from './package.json' assert { type: 'json' };
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: !isProduction,
-    },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: !isProduction,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ tsconfig: './tsconfig.json' }),
-    terser(),
-  ],
-  external: ['react', 'react-dom'],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: !isProduction,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: !isProduction,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser(),
+    ],
+    external: ['react', 'react-dom'],
+  },
+  {
+    input: 'dist/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts.default()],
+  },
+];
